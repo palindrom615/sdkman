ec9db1b5f664f60500c534e08f7f4881bf354e1c
Jang Whemoon <palindrom615@gmail.com>
2019-06-09 19:56:12 +0900

    Add multiplatform support

--- conf/conf.go
+++ conf/conf.go
@@ -3,6 +3,7 @@ package conf
 import (
 	"os"
 	"path"
+	"runtime"
 	"sync"
 )
 
@@ -19,10 +20,14 @@ var once sync.Once
 func GetConf() *config {
 	once.Do(func() {
 		home, _ := os.UserHomeDir()
+		platform := runtime.GOOS
+		if platform == "windows" {
+			platform = "msys_nt-10.0"
+		}
 		envInstance = &config{
 			Dir:      path.Join(home, ".sdkman"),
 			Api:      "https://api.sdkman.io/2",
-			Platform: "MSYS_NT-10.0",
+			Platform: platform,
 			Insecure: false,
 		}
 	})
