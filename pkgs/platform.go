d98b4230df957c22680b38f46590c0d62dac6f67
Jang Whemoon <palindrom615@gmail.com>
2021-08-19 13:47:26 +0900

    Add macos m1 support

---
+++ pkgs/platform.go
@@ -0,0 +1,33 @@
+package pkgs
+
+import "runtime"
+
+func Platform() string {
+	platform := runtime.GOOS
+	isx64 := runtime.GOARCH == "amd64"
+	isx86 := runtime.GOARCH == "386" || runtime.GOARCH == "amd64p32"
+	isArm := runtime.GOARCH == "arm"
+	isArm64 := runtime.GOARCH == "arm64"
+
+	if platform == "windows" {
+		platform = "MSYS_NT-10.0"
+		if isx86 {
+			platform = "MINGW32_NT-6.2"
+		}
+	} else {
+		if isx86 {
+			platform += "x32"
+		}
+		if isx64 {
+			platform += "x64"
+		}
+		if isArm {
+			platform += "arm32"
+		}
+		if isArm64 {
+			platform += "arm64"
+		}
+	}
+
+	return platform
+}
--- pkgs/utils.go
+++ pkgs/utils.go
@@ -23,21 +23,6 @@ func MkdirIfNotExist(root string) error {
 	return os.MkdirAll(arcDir, os.ModeDir|os.ModePerm)
 }
 
-func Platform() string {
-	platform := runtime.GOOS
-	is32bit := runtime.GOARCH == "386" || runtime.GOARCH == "amd64p32"
-
-	if platform == "windows" {
-		platform = "MSYS_NT-10.0"
-		if is32bit {
-			platform = "MINGW32_NT-6.2"
-		}
-	} else if platform == "linux" && is32bit {
-		platform += "32"
-	}
-	return platform
-}
-
 func Pager(pages io.ReadCloser) {
 	pager := os.Getenv("PAGER")
 
